8cf29188fd5784953d14ad99f890804ab11d5ca2
Jim Zhang <jim.zhang@kaleido.io>
2021-06-29 21:54:07 -0400

    Added support for dynamically calculated hash using HASH:idx parameter syntax

--- pkg/kldexerciser/kldcompiler.go
+++ pkg/kldexerciser/kldcompiler.go
@@ -16,6 +16,7 @@ package kldexerciser
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"math/big"
@@ -58,19 +59,38 @@ func GenerateTypedArgs(abi abi.ABI, methodName string, strargs []string, totalBa
 		strval := strargs[idx]
 		switch inputArg.Type.String() {
 		case "string":
-			typedArgs = append(typedArgs, strval)
+			if strings.Contains(strval, "HASH:") {
+				// special parameter that needs a hash to be calculated based on the index following the prefix
+				idxstr := strings.TrimPrefix(strval, "HASH:")
+				idx, err := strconv.Atoi(idxstr)
+				if err != nil {
+					fmt.Printf("Failed to parse index of the HASH: %s\n", strval)
+					return nil, fmt.Errorf("Failed to parse index of the HASH: %s. %s", strval, err)
+				}
+				targetVal := strargs[idx]
+				if strings.Contains(targetVal, "RANGE:") {
+					resolved, err := resolveRangeArg(targetVal, workerIndex, totalBatches, batchSize, batch, index)
+					if err != nil {
+						return nil, err
+					}
+					targetVal = resolved
+				}
+
+				h := sha256.New()
+				h.Write([]byte(targetVal))
+				hash := fmt.Sprintf("%x", h.Sum(nil))
+				typedArgs = append(typedArgs, hash)
+			} else {
+				typedArgs = append(typedArgs, strval)
+			}
 			break
 		case "int256", "uint256":
 			if strings.Contains(strval, "RANGE:") {
-				start := strings.TrimPrefix(strval, "RANGE:")
-				rand.Seed(time.Now().UnixNano())
-				min, err := strconv.Atoi(start)
+				resolved, err := resolveRangeArg(strval, workerIndex, totalBatches, batchSize, batch, index)
 				if err != nil {
-					fmt.Printf("Failed to parse start of the RANGE: %s\n", strval)
-					return nil, fmt.Errorf("Failed to parse start of the RANGE: %s. %s\n", strval, err)
+					return nil, err
 				}
-				strval = strconv.Itoa(min + workerIndex*totalBatches*batchSize + batch*batchSize + index)
-				fmt.Printf("Calculated RANGE index for worker %d patch %d index %d: %s\n", workerIndex, batch, index, strval)
+				strval = resolved
 			}
 			bigInt := big.NewInt(0)
 			if _, ok := bigInt.SetString(strval, 10); !ok {
@@ -172,4 +192,17 @@ func (c *CompiledSolidity) PackCall(totalBatches, batch, batchSize, index, worke
 		return nil, fmt.Errorf("Packing arguments %s for call %s: %s", c.args, c.method, err)
 	}
 	return packedCall, nil
+}
+
+func resolveRangeArg(strval string, workerIndex, totalBatches, batchSize, batch, index int) (string, error) {
+	start := strings.TrimPrefix(strval, "RANGE:")
+	rand.Seed(time.Now().UnixNano())
+	min, err := strconv.Atoi(start)
+	if err != nil {
+		fmt.Printf("Failed to parse start of the RANGE: %s\n", strval)
+		return "", fmt.Errorf("Failed to parse start of the RANGE: %s. %s", strval, err)
+	}
+	strval = strconv.Itoa(min + workerIndex*totalBatches*batchSize + batch*batchSize + index)
+	fmt.Printf("Calculated RANGE index for worker %d patch %d index %d: %s\n", workerIndex, batch, index, strval)
+	return strval, nil
 }
